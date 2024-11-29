d5b54e2653248dbaa8f59ff3afe9a4165ca05fa3
Eunsu Park <phd.choux@gmail.com>
2024-11-29 11:41:07 +0900

    Update save_tables.py

--- acquisition/save_tables.py
+++ acquisition/save_tables.py
@@ -1,6 +1,6 @@
 from utils import save_correction_table, save_pointing_table, save_psfs
 
-work_dir = "/Users/eunsu/Workspace/aia_pixel_dl"
+work_dir = "/media/eunsu/870EVO_1"
 
 save_correction_table(f"{work_dir}/correction_table.pkl")
 save_pointing_table(f"{work_dir}/pointing_table.pkl")

6137dfc6c2a031cfc530635abb8f580102ce2b42
Eunsu Park <phd.choux@gmail.com>
2025-03-10 13:08:31 +0900

    Update run.sh

--- dem-iterative-network/run.sh
+++ dem-iterative-network/run.sh
@@ -2,8 +2,8 @@
 
 
 python train.py \
-    --device cpu \
-    --data_file_path /Users/eunsu/Dataset/pixel/train/aia.euv.2011-01-01-00-00-00.h5 \
-    --save_root /Users/eunsu/Result/dine \
+    --device cuda \
+    --data_file_path $HOME/Dataset/pixel/train/aia.euv.2011-01-01-00-00-00.h5 \
+    --save_root $HOME/eunsu/Result/dine \
     --model_type pixel \
     --experiment_name test \

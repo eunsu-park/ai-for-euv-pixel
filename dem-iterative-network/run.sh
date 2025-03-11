a12f1332b218ae87789f9a74a2b7880b75bd5118
Eunsu Park <phd.choux@gmail.com>
2025-03-11 10:46:31 +0900

    Update run.sh

--- dem-iterative-network/run.sh
+++ dem-iterative-network/run.sh
@@ -14,7 +14,7 @@ nohup python train.py \
     --device cuda \
     --data_file_path $HOME/Dataset/pixel/train/aia.euv.2011-01-01-00-00-00.h5 \
     --save_root $HOME/Result/dine \
-    --model_type pixel \
+    --model_type conv \
     --experiment_name conv_logcosh \
     --loss_type log_cosh \
     --metric_type mae \

374bb5ffde3c1d183371daff5d68737f1ca8db82
Eunsu Park <phd.choux@gmail.com>
2025-03-14 11:53:11 +0900

    update deep

--- dem-iterative-network/run.sh
+++ dem-iterative-network/run.sh
@@ -4,18 +4,18 @@ nohup python train.py \
     --data_file_path $HOME/Dataset/pixel/train/aia.euv.2011-01-01-00-00-00.h5 \
     --save_root $HOME/Result/dine \
     --model_type pixel \
-    --experiment_name pixel_logcos \
+    --experiment_name deep_logcosh \
     --loss_type log_cosh \
     --metric_type mae \
     & \
 
 
-nohup python train.py \
-    --device cuda \
-    --data_file_path $HOME/Dataset/pixel/train/aia.euv.2011-01-01-00-00-00.h5 \
-    --save_root $HOME/Result/dine \
-    --model_type conv \
-    --experiment_name conv_logcosh \
-    --loss_type log_cosh \
-    --metric_type mae \
-    & \
+# nohup python train.py \
+#     --device cuda \
+#     --data_file_path $HOME/Dataset/pixel/train/aia.euv.2011-01-01-00-00-00.h5 \
+#     --save_root $HOME/Result/dine \
+#     --model_type conv \
+#     --experiment_name conv_logcosh \
+#     --loss_type log_cosh \
+#     --metric_type mae \
+#     & \
